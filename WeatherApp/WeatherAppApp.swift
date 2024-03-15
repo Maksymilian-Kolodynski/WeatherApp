d8964f60e5619b1f6bfbfa82ce1d184120f68abc
Maciej Maśka <maskamaciej@gmail.com>
2024-03-15 17:28:37 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Maciej Maśka on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
