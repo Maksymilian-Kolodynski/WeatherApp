ed3d8d003513f13740fb1a6e6d77632cee4d9bbf
Maks <maxttr8@gmail.com>
2024-07-04 01:22:01 +0200

    first commit

--- WeatherApp/WeatherAppApp.swift
+++ WeatherApp/WeatherAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  WeatherAppApp.swift
-//  WeatherApp
-//
-//  Created by Maciej Maśka on 15/03/2024.
-//
-
 import SwiftUI
 
 @main
 struct WeatherAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
